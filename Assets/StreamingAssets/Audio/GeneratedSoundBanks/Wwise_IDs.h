d98adf02e188b721878ec07ca7f12f799bca2e89
Permadeaf <101829691+Permadeaf@users.noreply.github.com>
2022-04-16 17:09:29 -0500

    Converted to reduce soundbank size

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,49 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID DIG = 445985471U;
+        static const AkUniqueID POSTMUSIC = 3750377736U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DIGTIME = 1136081764U;
+        static const AkUniqueID PLAYERDEPTH = 1755200717U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID ALLINONE = 2858921253U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MSTRINGS = 630186068U;
+        static const AkUniqueID ROOTS = 1510358672U;
+        static const AkUniqueID STRINGS = 3363523641U;
+        static const AkUniqueID SYNTHCHORDS = 3923599172U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
