d46ce3403f319dab724dcb448bfff78c525ff39b
Roosevelt Boyland III <treyboyland@aol.com>
2022-04-19 15:43:47 -0500

    Setting up WWise event call

--- Assets/Scripts/RainTrigger.cs
+++ Assets/Scripts/RainTrigger.cs
@@ -7,6 +7,9 @@ public class RainTrigger : MonoBehaviour
     [SerializeField]
     ParticleSystem rain;
 
+    [SerializeField]
+    AK.Wwise.Event rainDropEvent;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +26,11 @@ public class RainTrigger : MonoBehaviour
     {
         List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
         int numCollisionEvents = rain.GetCollisionEvents(other, collisionEvents);
+
+        for(int i = 0; i < numCollisionEvents; i++)
+        {
+            rainDropEvent.Post(gameObject);
+        }
 
         //Debug.LogWarning("Particles hit this frame: " + numCollisionEvents);
     }
