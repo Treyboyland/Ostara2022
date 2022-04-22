7caecfdb9bf711ee8e4973e2c79f30008199378a
Permadeaf <101829691+Permadeaf@users.noreply.github.com>
2022-04-21 20:17:50 -0500

    Generated soundbank

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,14 +14,17 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID DIG = 445985471U;
+        static const AkUniqueID DIRTFALL = 583926761U;
+        static const AkUniqueID ENEMYGROWL = 3621073518U;
+        static const AkUniqueID ENEMYMOAN = 3663202250U;
         static const AkUniqueID POSTMUSIC = 3750377736U;
-        static const AkUniqueID RAINIMPACT = 344026725U;
         static const AkUniqueID STORMPLAY = 2951963734U;
         static const AkUniqueID STORMSTOP = 3723974632U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS
     {
+        static const AkUniqueID BACKUPMETER = 1548096950U;
         static const AkUniqueID DIGTIME = 1136081764U;
         static const AkUniqueID PLAYERDEPTH = 1755200717U;
         static const AkUniqueID PLAYERDEPTHSLEWED = 1655984259U;
@@ -41,13 +44,13 @@ namespace AK
         static const AkUniqueID MSTRINGS = 630186068U;
         static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID MUSIC_AND_AMBIENCE = 1458896567U;
+        static const AkUniqueID MUSIC_BACKUP = 842978000U;
+        static const AkUniqueID MUSICMAIN = 374987273U;
         static const AkUniqueID RAINSEQUENCE = 3533120928U;
         static const AkUniqueID ROOTS = 1510358672U;
         static const AkUniqueID SFX = 393239870U;
         static const AkUniqueID STORM = 1686739424U;
         static const AkUniqueID STRINGS = 3363523641U;
-        static const AkUniqueID SYNTHCHORDS = 3923599172U;
-        static const AkUniqueID SYNTHHIGH = 289101121U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
